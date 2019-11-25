b9c57aafa02dd39c53ddd68160950dbac84af6a7
ON7RX <on7rx@sis.be>
2019-11-25 13:48:43 +0100

    typo

--- osmo-tetra/pdus.c
+++ osmo-tetra/pdus.c
@@ -136,7 +136,7 @@ void acc_pdu_18()
 	*/
 
 	bitvec_set_uint(&bv, 0, 2);	// alignment (<<2)
-	bitvec_set_uint(&bv, 0, 2);	// Uplink access rights: common and assigned
+	bitvec_set_uint(&bv, 0, 2);	// Uplink access rights: common
 	bitvec_set_uint(&bv, 1, 6);	// Access field 1
 	bitvec_set_uint(&bv, 0, 6);	// Access field 2
 
