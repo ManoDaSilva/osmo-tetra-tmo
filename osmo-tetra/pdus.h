b0723d751330003109fde52aa6115d7890175841
ON6RF <manoel.casquilho@gmail.com>
2019-11-26 09:05:00 +0100

    fixed unused function

--- osmo-tetra/pdus.h
+++ osmo-tetra/pdus.h
@@ -9,5 +9,6 @@ extern uint8_t pdu_schf[268];
 
 void sync_pdu(const uint16_t cc, const uint8_t mn, const uint8_t fn, const uint8_t tn, const uint8_t mcc, const uint16_t mnc);
 void sysinfo_pdu(const uint16_t hn);
+void null_pdu();
 void acc_pdu();
 void acc_pdu_18();
