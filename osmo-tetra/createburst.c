0a7a61cec72e43ce9288b44292a8415a5579444d
ON7RX <on7rx@sis.be>
2019-11-26 10:18:47 +0100

    remake build_ncdb (with MAC-DATA PDU) and apply NCDB on each frame (TS4)

--- osmo-tetra/createburst.c
+++ osmo-tetra/createburst.c
@@ -60,74 +60,116 @@ void tp_sap_udata_ind(enum tp_sap_data_type type, const uint8_t *bits, unsigned 
 {
 }
 
-/* Build a full 'Normal continuous downlink burst' from SYSINFO-PDU and SYNC-PDU, with a SCH/F logical channel */
-void build_ncdb_schf(uint8_t *buf, const uint8_t fn)
+/* Build a full 'Normal continuous downlink burst'
+ * from SYSINFO-PDU and SYNC-PDU, with a MAC-DATA PDU (in ??? channel) */
+void build_ncdb(uint8_t *buf, const uint8_t fn)
 {
-	/* input: 268 type-1 bits */
-	uint8_t type2[284];
-	uint8_t master[284*4];
-	uint8_t type3[432];
-	uint8_t type4[432];
-	uint8_t type5[432];
+	uint8_t sb1_type2[140];
+	uint8_t sb1_master[216*4];
+	uint8_t sb1_type3[216];
+	uint8_t sb1_type4[216];
+	uint8_t sb1_type5[216];
+
+	uint8_t sb2_type2[140];
+	uint8_t sb2_master[216*4];
+	uint8_t sb2_type3[216];
+	uint8_t sb2_type4[216];
+	uint8_t sb2_type5[216];
+
 	uint8_t bb_type5[30];
-	uint8_t burst[255*2];
 	uint16_t crc;
 	uint8_t *cur;
 	uint32_t bb_rm3014, bb_rm3014_be;
 
 	uint32_t scramb_init = tetra_scramb_get_init(MCC, MNC, CC);
 
-	memset(type2, 0, sizeof(type2));
-	cur = type2;
+	memset(sb1_type2, 0, sizeof(sb1_type2));
+	cur = sb1_type2;
 
-	/* Use pdu_schf from pdus.c: type-1 bits */
-	cur += osmo_pbit2ubit(type2, pdu_schf, 268);
+	// Use MAC-DATA PDU from pdus.c
+	cur += osmo_pbit2ubit(sb1_type2, pdu_mac_data, 124);
+	//printf("MAC-DATA PDU: %s\n", osmo_ubit_dump(sb1_type2, 124));
 
-	crc = ~crc16_ccitt_bits(type2, 268);
+	// calculate checksum is useless for NULL PDU
+	// Run it through CRC16-CCITT
+	/*
+	crc = ~crc16_ccitt_bits(sb1_type2, 124);
 	crc = swap16(crc);
 	cur += osmo_pbit2ubit(cur, (uint8_t *) &crc, 16);
 
-	/* Append 4 tail bits: type-2 bits */
+	// Append 4 tail bits: type-2 bits
 	cur += 4;
+	*/
 
-	/* Run rate 2/3 RCPC code: type-3 bits*/
+	/* Run rate 2/3 RCPC code: type-3 bits */
 	{
 		struct conv_enc_state *ces = calloc(1, sizeof(*ces));
 		conv_enc_init(ces);
-		conv_enc_input(ces, type2, 288, master);
-		get_punctured_rate(TETRA_RCPC_PUNCT_2_3, master, 432, type3);
+		conv_enc_input(ces, sb1_type2, 144, sb1_master);
+		get_punctured_rate(TETRA_RCPC_PUNCT_2_3, sb1_master, 216, sb1_type3);
 		free(ces);
 	}
 
-	/* Run (432,103) block interleaving: type-4 bits */
-	block_interleave(432, 103, type3, type4);
+	// Run (216,101) block interleaving: type-4 bits
+	block_interleave(216, 101, sb1_type3, sb1_type4);
 
-	memcpy(type5, type4, 432);
+	memcpy(sb1_type5, sb1_type4, 216);
 
-	/* Run scrambling (all-zero): type-5 bits */
-	tetra_scramb_bits(SCRAMB_INIT, type5, 432);
-	//printf("Scrambled block 1 bits (SCH/F): %s\n", osmo_ubit_dump(type5, 216));
+	// Run scrambling (all-zero): type-5 bits
+	tetra_scramb_bits(scramb_init, sb1_type5, 216);
+	//printf("Scrambled block 1 bits (MAC-DATA PDU): %s\n", osmo_ubit_dump(sb1_type5, 216));
 
-	/* Use pdu_acc_ass/pdu_acc_ass_18 from pdus.c */
+	memset(sb2_type2, 0, sizeof(sb2_type2));
+	cur = sb2_type2;
+
+	// Use pdu_sysinfo from pdus.c
+	cur += osmo_pbit2ubit(sb2_type2, pdu_sysinfo, 124);
+
+	// Run it through CRC16-CCITT
+	crc = ~crc16_ccitt_bits(sb2_type2, 124);
+	crc = swap16(crc);
+	cur += osmo_pbit2ubit(cur, (uint8_t *) &crc, 16);
+
+	// Append 4 tail bits: type-2 bits
+	cur += 4;
+
+	// Run rate 2/3 RCPC code: type-3 bits
+	{
+		struct conv_enc_state *ces = calloc(1, sizeof(*ces));
+		conv_enc_init(ces);
+		conv_enc_input(ces, sb2_type2, 144, sb2_master);
+		get_punctured_rate(TETRA_RCPC_PUNCT_2_3, sb2_master, 216, sb2_type3);
+		free(ces);
+	}
+
+	// Run (216,101) block interleaving: type-4 bits
+	block_interleave(216, 101, sb2_type3, sb2_type4);
+
+	memcpy(sb2_type5, sb2_type4, 216);
+
+	// Run scrambling (all-zero): type-5 bits
+	tetra_scramb_bits(scramb_init, sb2_type5, 216);
+	//printf("Scrambled block 2 bits (BNCH): %s\n", osmo_ubit_dump(sb2_type5, 216));
+
+	// Use pdu_acc_ass/pdu_acc_ass_18 from pdus.c
 	uint8_t *bb_type1 = (uint8_t *)(fn < 18 ? pdu_acc_ass : pdu_acc_ass_18); // ACCESS-ASSIGN
-	/* Run it through (30,14) RM code: type-2=3=4 bits */
+	// Run it through (30,14) RM code: type-2=3=4 bits
 	bb_rm3014 = tetra_rm3014_compute(*(bb_type1) << 8 | *(bb_type1 + 1));
-	/* convert to big endian */
+	// convert to big endian
 	bb_rm3014_be = htonl(bb_rm3014);
-	/* shift two bits left as it is only a 30 bit value */
+	// shift two bits left as it is only a 30 bit value
 	bb_rm3014_be <<= 2;
 	osmo_pbit2ubit(bb_type5, (uint8_t *) &bb_rm3014_be, 30);
 
-	/* Run scrambling (all-zero): type-5 bits */
+	// Run scrambling (all-zero): type-5 bits
 	tetra_scramb_bits(scramb_init, bb_type5, 30);
 
 	//printf("Scrambled broadcast bits (AACH): %s\n", osmo_ubit_dump(bb_type5, 30));
-	//printf("Scrambled block 2 bits (SCH/F): %s\n", osmo_ubit_dump(type5+216, 216));
 
-	/* Finally, hand it into the physical layer */
-	build_norm_c_d_burst(buf, type5, bb_type5, type5+216, 0);
+	// Finally, hand it into the physical layer
+	build_norm_c_d_burst(buf, sb1_type5, bb_type5, sb2_type5, 0);
 
-	//printf("Normal continuous downlink burst (NCDB): %s\n", osmo_ubit_dump(burst, 255*2));
+	//printf("Normal continuous downlink burst (NCDB): %s\n", osmo_ubit_dump(buf, BLEN));
 }
 
 /* Build a full 'Synchronization continuous downlink burst' from SYSINFO-PDU and SYNC-PDU */
@@ -248,6 +290,7 @@ int main(int argc, char **argv)
 
 	tetra_rm3014_init();
 	sysinfo_pdu(cur_hn);
+	mac_data_pdu();
 	acc_pdu();
 	acc_pdu_18();
 
@@ -258,7 +301,8 @@ int main(int argc, char **argv)
 		//printf("%02u/%02u/%02u Hyperframe %05u\n", cur_mn, cur_fn, cur_tn, cur_hn);
 		/* GENERATE THE BURST HERE */
 		//printf("SCDB BURST\n");
-		build_scdb(bp, cur_fn);
+		if (cur_tn < 3) build_scdb(bp, cur_fn);
+		else build_ncdb(bp, cur_fn);
 		//printf("OUTPUT: %s\n", osmo_ubit_dump(burst, BLEN));
 		printf("%s", osmo_ubit_dump(burst, BLEN));
 
